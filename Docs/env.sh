d96871be846f6092cfbc523b7cb332854fce2336
sixdigit <mog@sixdigit.net>
2022-07-30 22:11:42 -0400

    env var

---
+++ Docs/env.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+export DATABASE='name of your database'
+export DB_USER='user name of your database'
+export DB_PASS='password of your database'
+export DB_HOST='hosting address of the database'
+export DB_PORT='port of the database'
